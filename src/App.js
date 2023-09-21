cf8e50415ba053c5632f186a980636e65971e1fa
Ibrahim Ismail <ismail.ibrahimadeiza@gmail.com>
2023-09-21 15:23:22 +0100

    Modal Done

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
-import { LargeAuthorListItem } from './components/authors/LargeListItems';
-import { SmallAuthorListItem } from './components/authors/SmallListItems';
-import { SmallBookListItem } from './components/books/SmallListItems';
-import { RegularList } from './components/lists/Regular';
-
-import { authors } from './data/authors';
 import { books } from './data/books';
 import { LargeBookListItem } from './components/books/LargeListItems';
-import { NumberedList } from './components/lists/Numbered.jsx';
+import { Modal } from './components/Modal';
 
 function App() {
   return (
     <>
-      <RegularList
-        items={authors}
-        sourceName={'author'}
-        ItemComponent={SmallAuthorListItem}
-      />
-      <NumberedList
-        items={authors}
-        sourceName={'author'}
-        ItemComponent={LargeAuthorListItem}
-      />
-      <RegularList
-        items={books}
-        sourceName={'book'}
-        ItemComponent={SmallBookListItem}
-      />
-      <NumberedList
-        items={books}
-        sourceName={'book'}
-        ItemComponent={LargeBookListItem}
-      />
+      <Modal>
+        <LargeBookListItem book={books[0]} />
+      </Modal>
     </>
   );
 }
