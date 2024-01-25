7f412f25a16a60f6aed5252951c2b8b771b3c03e
Ibrahim Ismail <ismail.ibrahimadeiza@gmail.com>
2024-01-25 21:59:12 +0100

    layout components for fetching data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import { LargeBookListItem } from './components/books/largeListItems';
-import { Modal } from './components/modal';
-import { books } from './data/books';
+import { UserInfo } from './components/user-info';
+import axios from 'axios';
+import { DataSourceWIthRender } from './components/data-source-with-render';
+import { DataSource } from './components/data-source';
 
 function App() {
+  const getDataFromServer = async (url) => {
+    const response = await axios.get(url);
+    return response.data;
+  };
+
+  const getDataFromLocalStorage = (key) => () => {
+    return localStorage.getItem(key);
+  };
+
+  const Message = ({ msg }) => {
+    return <h1>{msg}</h1>;
+  };
+
   return (
     <>
-      <Modal>
-        <LargeBookListItem book={books[0]} />
-      </Modal>
+      <DataSource
+        getData={() => getDataFromServer('/users/2')}
+        resourceName={'user'}
+      >
+        <UserInfo />
+      </DataSource>
+      {/* <DataSourceWIthRender
+        getData={() => getDataFromServer('/users/2')}
+        render={(resource) => <UserInfo user={resource} />}
+      ></DataSourceWIthRender> */}
+
+      <DataSource
+        getData={() => getDataFromLocalStorage('test')}
+        resourceName={'msg'}
+      >
+        <Message />
+      </DataSource>
     </>
   );
 }
